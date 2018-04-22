51d202b1617571eb915b968bdea65d4e23d4beb7
urvashimahto16 <urvashi.mahto16@gmail.com>
2018-04-22 04:51:26 -0700

    Update purpose.js

--- purpose.js
+++ purpose.js
@@ -2,16 +2,34 @@
 'use strict';
 
 var boolVisited = false;
+var boolPurposeWanted = true;
 
 function save(){
-var purposeInput =document.getElementById('purposeBox').value;
-localStorage.setItem("ThePurpose", purposeInput); // save the item
+    var purposeInput =document.getElementById('purposeBox').value;
+    document.getElementById("logg")= purposeInput;
+    localStorage.setItem("ThePurpose", purposeInput); // save the item
+    //toFacebook();
 }
+document.getElementById("purposeEntered").onclick = save();
 
-function toFacebook() {
+/*$(function(){
+    $('purposeEntered').click(function(){
+        var purposeInnerds = $("#purposeBox").val();
+        if (purposeInnerds){
+            chrome.storage.sync.set({"purposeBox" : purposeInnerds}, toFacebook())
+        }
+    })
+})*/
+
+/*function toFacebook() {
    if (!boolVisited) {
-    window.location.assign("https://www.facebook.com");
-    boolVisited = true;
-    document.getElementById("purpose").innerHTML = save();
+        window.location.assign("https://www.facebook.com");
+        boolVisited = true;
     }
+}*/
+
+function setPurposeWantedToFalse(){
+    boolPurposeWanted = false;
+    localStorage.setItem("ThePurpose", "none");
 }
+document.getElementById("noPurpose").onclick = setPurposeWantedToFalse();
