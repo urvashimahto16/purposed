03b453e1804c2b5222b44ab648e790e9d7f8c61f
urvashimahto16 <urvashi.mahto16@gmail.com>
2018-04-21 21:01:15 -0700

    Add files via upload

---
+++ purpose.js
@@ -0,0 +1,17 @@
+
+'use strict';
+
+var boolVisited = false;
+
+function save(){
+var purposeInput =document.getElementById('purposeBox').value;
+localStorage.setItem("ThePurpose", purposeInput); // save the item
+}
+
+function toFacebook() {
+   if (!boolVisited) {
+    window.location.assign("https://www.w3schools.com");
+    boolVisited = true;
+    document.getElementById("purpose").innerHTML = save();
+    }
+}
